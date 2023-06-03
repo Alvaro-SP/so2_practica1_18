1d79a874d24677a1010515357501b2b092687a09
Alvaro-SP <socop2412@gmail.com>
2023-06-03 12:03:29 -0600

    change cpu

--- module/cpu_202000194.c
+++ module/cpu_202000194.c
@@ -72,7 +72,7 @@ static int escribir_archivo(struct seq_file *archivo, void *v)
     guest = guest_nice = 0;
     getboottime64(&boottime);
     /* shift boot timestamp according to the timens offset */
-    timens_sub_boottime(&boottime);
+    ktime_get_boottime(&boottime);
     for_each_possible_cpu(i)
     {
         struct kernel_cpustat kcpustat;
@@ -84,7 +84,7 @@ static int escribir_archivo(struct seq_file *archivo, void *v)
         nice += cpustat[CPUTIME_NICE];
         system += cpustat[CPUTIME_SYSTEM];
         idle += get_idle_time(&kcpustat, i);
-        iowait += get_iowait_time(&kcpustat, i);
+        // iowait += get_iowait_time(&kcpustat, i);
         irq += cpustat[CPUTIME_IRQ];
         softirq += cpustat[CPUTIME_SOFTIRQ];
         steal += cpustat[CPUTIME_STEAL];
@@ -94,7 +94,7 @@ static int escribir_archivo(struct seq_file *archivo, void *v)
         sum += arch_irq_stat_cpu(i);
     }
 
-    printk(KERN_INFO "CPU Percent: %d%%\n", cpu_percent);
+    printk(KERN_INFO "CPU Percent: %d%%\n", cpu_usage);
 
     si_meminfo(&info);
 
