8c563b5e97d4bb3509be25682e7785fefe6af876
Alvaro-SP <socop2412@gmail.com>
2023-06-03 20:22:38 -0600

    added module ram

--- module/eje.sh
+++ module/eje.sh
@@ -4,14 +4,14 @@ ruta=$(pwd)
 
 # Moverse a la ruta del repositorio Git
 cd $ruta
-sudo rmmod cpu_202000194
-sudo rmmod ram_202000194
+sudo rmmod cpu_grupo18
+sudo rmmod mem_grupo18
 make clean
 make all
-sudo insmod cpu_202000194.ko
-sudo insmod ram_202000194.ko
-cat /proc/cpu_202000194
-cat /proc/ram_202000194
+sudo insmod cpu_grupo18.ko
+sudo insmod mem_grupo18.ko
+cat /proc/cpu_grupo18
+cat /proc/mem_grupo18
 
 # ! EJECUTAR EN LA VM GCP
 # cd "/home/alvaro24_ingenieria/so2_practica1_18/module"
